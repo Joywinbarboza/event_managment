214f7c2b5eb3b90f72c5d5aa262e68acc746b7f7
Joywinbarboza <joywinbarboza420@gmail.com>
2024-07-01 22:32:06 +0530

    fixed eslint apos landing page button

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 function Home() {
-  return (<>
-    <div className="bg-yellow-400 text-black text-center h-20">Navbar Here</div>
-    <div className='bg-blue-600 font-permanentMarker text-3xl'>This is where we create <span className='text-red-500'>slider</span> and <span className='text-red-500'>search</span> And add a <span className='text-yellow-500'>Navbar Component</span> which is common for all except login and landing page</div>
+  const router = useRouter();
+
+  const goToLandingPage = () => {
+    // Navigate to signup page
+    router.push("/");
+  };
+
+  return (
+    <>
+      <div className="bg-yellow-400 text-black text-center h-20">
+        Navbar Here
+      </div>
+      <div className="bg-blue-600 font-permanentMarker text-3xl">
+        This is where we create <span className="text-red-500">slider</span> and{" "}
+        <span className="text-red-500">search</span> And add a{" "}
+        <span className="text-yellow-500">Navbar Component</span> which is
+        common for all except login and landing page
+      </div>
+      <center>
+        <button
+          className="h-96 w-960 items-center bg-purple-500 flex"
+          onClick={goToLandingPage}
+        >
+          go to landing page
+        </button>
+      </center>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
